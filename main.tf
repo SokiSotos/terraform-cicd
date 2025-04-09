27fa5b640c868c01f758b5610b0e1c46831ee400
SokiSotos <115071164+SokiSotos@users.noreply.github.com>
2025-04-09 12:39:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   s3_use_path_style           = true
+  skip_requesting_account_id  = true
   endpoints {
     s3 = "http://localhost:4566"
   }
